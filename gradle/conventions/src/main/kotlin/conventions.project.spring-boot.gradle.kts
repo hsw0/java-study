98b33ab4a0e00ffaa1701a71fb8ddb10b64d9c84
hsw <hsw@syscall.io>
2023-07-02 20:50:04 +0900

    spring-boot: Kotlin kapt 설정
    https://kotlinlang.org/docs/kapt.html

--- gradle/conventions/src/main/kotlin/conventions.project.spring-boot.gradle.kts
+++ gradle/conventions/src/main/kotlin/conventions.project.spring-boot.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.KaptExtension
+
 /**
  * Spring Boot 에 의존하는 project convention
  *
@@ -8,12 +10,29 @@ plugins {
     id("conventions.project.jvm")
 }
 
+if (pluginManager.hasPlugin("org.jetbrains.kotlin.jvm")) {
+    logger.info("Spring Boot: Applying kotlin specific config")
+
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.jetbrains.kotlin.kapt")
+
+    configure<KaptExtension> {
+        showProcessorStats = true
+    }
+}
+
+val aptConfiguration = if (configurations.get("kapt") != null) {
+    "kapt"
+} else {
+    "annotationProcessor"
+}
+
 dependencies {
     implementation(platform("org.springframework.boot:spring-boot-dependencies"))
 
-    annotationProcessor("org.springframework:spring-context-indexer")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    annotationProcessor("org.springframework.boot:spring-boot-autoconfigure-processor")
+    add(aptConfiguration, "org.springframework:spring-context-indexer")
+    add(aptConfiguration, "org.springframework.boot:spring-boot-configuration-processor")
+    add(aptConfiguration, "org.springframework.boot:spring-boot-autoconfigure-processor")
 
     compileOnly("org.springframework:spring-core")
     compileOnly("org.springframework:spring-context")
@@ -21,9 +40,4 @@ dependencies {
     compileOnly("org.springframework.boot:spring-boot-autoconfigure")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-if (pluginManager.hasPlugin("org.jetbrains.kotlin.jvm")) {
-    logger.info("Spring Boot: Applying kotlin allopen (spring)")
-    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
 }
