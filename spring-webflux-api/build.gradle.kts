1a99a11148406c918186248f53d10bf1486d873f
hsw <hsw@syscall.io>
2023-07-03 18:44:41 +0900

    Add guava

--- spring-webflux-api/build.gradle.kts
+++ spring-webflux-api/build.gradle.kts
@@ -9,4 +9,5 @@ version = "1.0-SNAPSHOT"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.google.guava:guava")
 }
