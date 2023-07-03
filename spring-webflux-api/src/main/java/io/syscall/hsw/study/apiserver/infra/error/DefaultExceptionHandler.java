a1bf81e1537c264728f351d71fbc098799775edd
hsw <hsw@syscall.io>
2023-07-03 22:01:58 +0900

    DefaultExceptionHandler: Log exception

--- spring-webflux-api/src/main/java/io/syscall/hsw/study/apiserver/infra/error/DefaultExceptionHandler.java
+++ spring-webflux-api/src/main/java/io/syscall/hsw/study/apiserver/infra/error/DefaultExceptionHandler.java
@@ -1,7 +1,45 @@
 package io.syscall.hsw.study.apiserver.infra.error;
 
+import javax.annotation.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @ControllerAdvice
-public class DefaultExceptionHandler extends ResponseEntityExceptionHandler {}
+public class DefaultExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private final Logger log = LoggerFactory.getLogger(DefaultExceptionHandler.class);
+
+    @Override
+    protected Mono<ResponseEntity<Object>> handleExceptionInternal(
+            Exception ex,
+            @Nullable Object body,
+            @Nullable HttpHeaders headers,
+            HttpStatusCode status,
+            ServerWebExchange exchange) {
+        log(ex, exchange);
+        return super.handleExceptionInternal(ex, body, headers, status, exchange);
+    }
+
+    private void log(Exception ex, ServerWebExchange exchange) {
+        var request = exchange.getRequest();
+        var logEvent = log.atError()
+                .setCause(ex)
+                .setMessage("Exception!")
+                .addKeyValue("http.request.method", request.getMethod().name())
+                .addKeyValue("url.full", request.getURI());
+        if (exchange.getAttribute(HandlerMapping.BEST_MATCHING_HANDLER_ATTRIBUTE) instanceof HandlerMethod hm) {
+            logEvent = logEvent.addKeyValue("code.namespace", hm.getBeanType().getSimpleName())
+                    .addKeyValue("code.function", hm.getMethod().getName());
+        }
+        logEvent.log();
+    }
+}
