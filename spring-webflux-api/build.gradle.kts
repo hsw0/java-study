91e53b14da58a93f08b17624e1aec8214bdb554e
hsw <hsw@syscall.io>
2023-07-03 20:37:44 +0900

    api: Add reactor dev/test dependencies

--- spring-webflux-api/build.gradle.kts
+++ spring-webflux-api/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("conventions.project.kotlin")
     id("conventions.project.spring-boot-app")
+    id("io.syscall.gradle.plugin.devonly")
 }
 
 group = "io.syscall.hsw.study"
@@ -10,4 +11,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.google.guava:guava")
+
+    testImplementation("io.projectreactor:reactor-test")
+    devRuntimeOnly("io.projectreactor:reactor-tools")
+    testRuntimeOnly("io.projectreactor:reactor-tools")
+    compileOnly("io.projectreactor.tools:blockhound") // BlockHoundIntegration SPI
+    devRuntimeOnly("io.projectreactor.tools:blockhound")
+    testRuntimeOnly("io.projectreactor.tools:blockhound")
 }
