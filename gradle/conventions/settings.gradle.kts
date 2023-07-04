971b348a7b673720ee45ca1aa09f621ca5eaf726
hsw <hsw@syscall.io>
2023-07-04 19:05:11 +0900

    convention: 플러그인 내 version catalog 관련 주석 추가

--- gradle/conventions/settings.gradle.kts
+++ gradle/conventions/settings.gradle.kts
@@ -1,6 +1,8 @@
 rootProject.name = "project-conventions"
 
 dependencyResolutionManagement {
+    // 참고: Version catalog를 선언하더라도 build.gradle.kts가 아닌 플러그인 내에서는 typesafe accessor을 사용할 수 없다
+    // Make version catalogs accessible from precompiled script pluginshttps://github.com/gradle/gradle/issues/15383
     versionCatalogs {
         create("libs") {
             from(files("../libs.versions.toml"))
