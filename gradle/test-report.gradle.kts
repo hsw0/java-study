e2436b3bd15aac290a1fe79cf11af01ee52bf1af
hsw <hsw@syscall.io>
2023-07-10 13:05:46 +0900

    test-report: jacocoTestReport에서 오류 발생하는 프로젝트 예외 처리.

--- gradle/test-report.gradle.kts
+++ gradle/test-report.gradle.kts
@@ -15,12 +15,34 @@ plugins {
     `test-report-aggregation`
     `jacoco-report-aggregation`
 }
+/*
+얘들은 이상한 오류가 발생:
+**`java-test-fixtures` 플러그인이 있고 kotlin 플러그인이 없으면 발생한다**
+
+Could not determine the dependencies of task ':test-report:jacocoTestReport'.
+> Could not resolve all task dependencies for configuration ':test-report:aggregateCodeCoverageReportResults'.
+   > The consumer was configured to find a component, preferably in the form of class files. However we cannot choose between the following variants of project ${project.path}:
+       - Configuration '${project.path}:apiElements' variant classes declares a component, preferably in the form of class files:
+           - Unmatched attributes:
+               - Provides attribute 'artifactType' with value 'java-classes-directory' but the consumer didn't ask for it
+
+     The following variants were also considered but didn't match the requested attributes:
+       - Configuration '${project.path}:runtimeElements' variant resources:
+           - Incompatible because this component declares a component, preferably only the resources files and the consumer needed a component, preferably in the form of class files
+*/
+val jaCoCoExcludedProjects = setOf(
+    ":module:logging-support",
+    ":module:jpa-support",
+    ":module:reactor-support",
+    ":module:springboot-app-base",
+)
 
 val projectsList = rootProject.allprojects.filterNot { it.path == project.path }
 
 for (targetProject in projectsList) {
     evaluationDependsOn(targetProject.path)
 }
+
 
 afterEvaluate {
     dependencies {
