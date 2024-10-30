1cb7f6e50c3f25265aa9f38a8eb32e6536f82c4f
hsw <hsw@syscall.io>
2024-10-31 08:26:01 +0900

    update spring boot autoconfiguration

--- module/springboot-app-base/src/main/java/io/syscall/commons/module/appbase/common/CommonAppBaseAutoConfiguration.java
+++ module/springboot-app-base/src/main/java/io/syscall/commons/module/appbase/common/CommonAppBaseAutoConfiguration.java
@@ -1,8 +1,19 @@
 package io.syscall.commons.module.appbase.common;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.availability.ApplicationAvailabilityAutoConfiguration;
+import org.springframework.boot.autoconfigure.context.ConfigurationPropertiesAutoConfiguration;
+import org.springframework.boot.autoconfigure.context.LifecycleAutoConfiguration;
+import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @AutoConfiguration
+@ImportAutoConfiguration({
+    ConfigurationPropertiesAutoConfiguration.class,
+    PropertyPlaceholderAutoConfiguration.class,
+    LifecycleAutoConfiguration.class,
+    ApplicationAvailabilityAutoConfiguration.class,
+})
 @ComponentScan
 class CommonAppBaseAutoConfiguration {}
