0d9cb7b2e317da5ea0975fee88441613aa5fd499
hsw <1043930+hsw0@users.noreply.github.com>
2025-11-23 09:13:01 +0900

    gradle: DownloadDependenciesTask: 로직 개선
    * Configuration cache를 고려하여 내부 프로젝트끼리 Transitive 참조하는 것 제거
    * buildscript 의존성도 다운받도록. convention 플러그인까지는 못 따라감

--- gradle/conventions/src/main/kotlin/io/syscall/gradle/task/DownloadDependenciesTask.kt
+++ gradle/conventions/src/main/kotlin/io/syscall/gradle/task/DownloadDependenciesTask.kt
@@ -2,25 +2,37 @@ package io.syscall.gradle.task
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.artifacts.ResolvableDependencies
+import org.gradle.api.artifacts.component.ProjectComponentIdentifier
 import org.gradle.api.tasks.TaskAction
 
 abstract class DownloadDependenciesTask : DefaultTask() {
     private val projectName = project.name
-    private val configurations: Map<String, ResolvableDependencies>
+    private val allConfigurations: Map<String, ResolvableDependencies>
 
     init {
         group = "build setup"
         description = "Download all dependencies into the cache"
         notCompatibleWithConfigurationCache("Unsupported")
 
-        configurations = project.configurations.matching { it.isCanBeResolved }.associate { it.name to it.incoming }
+        val configurations =
+            project.configurations.matching { it.isCanBeResolved }.associate { it.name to it.incoming }
+        val buildConfigurations =
+            project.buildscript.configurations
+                .matching { it.isCanBeResolved }
+                .associate { "build-${it.name}" to it.incoming }
+        allConfigurations = configurations + buildConfigurations
     }
 
     @TaskAction
     fun action() {
-        for ((name, configuration) in configurations.entries) {
+        for ((name, configuration) in allConfigurations.entries) {
             logger.lifecycle("Resolving project: {} configuration: {}", projectName, name)
-            val artifacts = configuration.artifactView { lenient(true) }.artifacts
+            val artifacts =
+                configuration
+                    .artifactView {
+                        lenient(true)
+                        componentFilter { it !is ProjectComponentIdentifier }
+                    }.artifacts
             for (it in artifacts) {
                 logger.info(" Resolved artifact: {} => {}", it.id, it.file)
             }
