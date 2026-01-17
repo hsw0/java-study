514f438bb1c7390bb6a08fbc84a6cfcbb1c51229
hsw <1043930+hsw0@users.noreply.github.com>
2026-01-17 19:02:08 +0900

    apiserver: programmatic JFR recording

--- spring-webflux-api/src/main/kotlin/io/syscall/hsw/study/apiserver/ApiApplication.kt
+++ spring-webflux-api/src/main/kotlin/io/syscall/hsw/study/apiserver/ApiApplication.kt
@@ -1,11 +1,25 @@
 package io.syscall.hsw.study.apiserver
 
+import jdk.jfr.consumer.RecordingStream
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.aot.AbstractAotProcessor
+import org.springframework.context.support.DefaultLifecycleProcessor
+import org.springframework.core.metrics.jfr.FlightRecorderApplicationStartup
 
 @SpringBootApplication
 public class ApiApplication
 
 public fun main(args: Array<String>) {
-    runApplication<ApiApplication>(args = args)
+    if (System.getProperty(AbstractAotProcessor.AOT_PROCESSING) == null &&
+        System.getProperty(DefaultLifecycleProcessor.EXIT_PROPERTY_NAME) == null
+    ) {
+        val jfrConfig = jdk.jfr.Configuration.getConfiguration("default")
+        val jfrRecording = RecordingStream(jfrConfig)
+        jfrRecording.startAsync()
+    }
+
+    runApplication<ApiApplication>(args = args) {
+        applicationStartup = FlightRecorderApplicationStartup()
+    }
 }
